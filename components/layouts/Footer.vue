11e0d86997bed7c8500521491e84c68518f260ab
cryptomania9999 <124178855+cryptomania9999@users.noreply.github.com>
2024-09-10 17:29:45 +0300

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -13,22 +13,27 @@ onMounted(() => {
       <a
         href="/"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
-        title="Sink"
-      >Sink</a>
+        title="Ceramoza"
+      > <img
+                src="/logo-d.png"
+                alt="Ceramoza"
+                class="w-full h-full"
+              >
+      </a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://paraphgraphic.ir/"
         target="_blank"
-        title="HTML.ZONE"
+        title="ParaphGraphic"
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} Powered by ParaphGraphic
       </a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
       >
         <a
           ref="email"
-          href="mailto:sink.cool$miantiao.me"
+          href="mailto:info@ceramoza.com"
           title="Email"
           class="text-gray-400 hover:text-gray-500"
         >
@@ -38,7 +43,7 @@ onMounted(() => {
           />
         </a>
         <a
-          href="https://t.me/htmlzone"
+          href="https://t.me/ceramoza"
           target="_blank"
           title="Telegram"
           class="text-gray-400 hover:text-gray-500"
@@ -48,53 +53,7 @@ onMounted(() => {
             class="w-6 h-6"
           />
         </a>
-        <a
-          href="https://mt.ci"
-          target="_blank"
-          title="Blog"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Blog</span>
-          <BloggerIcon
-            class="w-6 h-6"
-          />
-        </a>
 
-        <a
-          href="https://x.com/0xKaiBi"
-          target="_blank"
-          title="Twitter"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Twitter</span>
-          <XIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://miantiao.me/@chi"
-          target="_blank"
-          title="Mastodon"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Mastodon</span>
-          <MastodonIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://github.com/ccbikai"
-          target="_blank"
-          title="GitHub"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">GitHub</span>
-          <GitHubIcon
-            class="w-6 h-6"
-          />
-        </a>
       </span>
     </div>
   </section>
